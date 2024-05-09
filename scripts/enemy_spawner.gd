b81815e2a30b82ea7c5588a93db64a83522995f3
Jake Lipson <jake_lipson@yahoo.com>
2024-05-09 03:17:50 -0400

    Added enemy spawner

---
+++ scripts/enemy_spawner.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var enemy_scene = preload("res://scenes/enemy.tscn")
+
+@onready var spawn_positions = $SpawnPositions
+@onready var timeout_notifier = $Timer
+
+func _ready():
+	timeout_notifier.connect("timeout", _timer_timeout)
+
+func _timer_timeout():
+	spawn_enemy()
+
+func spawn_enemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	
+	var enemy_instantce = enemy_scene.instantiate()
+	enemy_instantce.global_position = random_spawn_position.global_position
+	add_child(enemy_instantce)
