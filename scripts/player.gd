9beabf9c9111108a62bfdc7b24ad3a4753338893
Jake Lipson <jake_lipson@yahoo.com>
2024-05-09 02:36:28 -0400

    Added ability to shoot rockets
    instantiated rocket scenes from the player scene when the "shoot" action is triggered

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
 @export var speed = 300
+# TODO: Add rocket cooldown and related logic
+
+var rocket_scene = preload("res://scenes/rocket.tscn")
+
+@onready var rocket_container = $RocketContainer #get_node("RocketContainer")
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func _physics_process(delta):
 	velocity = Vector2(0,0)
@@ -18,3 +27,9 @@ func _physics_process(delta):
 	var screen_size = get_viewport_rect().size
 	
 	global_position = global_position.clamp(Vector2(0,0), screen_size)
+
+func shoot():
+	var rocket_instance = rocket_scene.instantiate()
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
+	rocket_instance.global_position.x += 80	
