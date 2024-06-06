fec0ee17e69da7f19d1ecf502fba7b9566295962
Jake Lipson <jake_lipson@yahoo.com>
2024-06-06 00:30:32 -0400

    Added sound effects
    Lesson 3.20

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,8 @@ var rocket_scene = preload("res://scenes/rocket.tscn")
 
 @onready var rocket_container = $RocketContainer #get_node("RocketContainer")
 
+@onready var rocket_shot_sound = $RocketShotSound
+
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -35,6 +37,7 @@ func shoot():
 	rocket_container.add_child(rocket_instance)
 	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80	
+	rocket_shot_sound.play()
 
 func take_damage():
 	emit_signal("took_damage")
