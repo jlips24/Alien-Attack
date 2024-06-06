74d6c487ca113bfc2b94702146e0acfa41349ae4
Jake Lipson <jake_lipson@yahoo.com>
2024-06-06 01:05:47 -0400

    Added path enemy to the spawner
    Lesson 3.23

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 signal enemy_spawned(enemy_instance)
+signal path_enemy_spawned(path_enemy_instance)
 
 var enemy_scene = preload("res://scenes/enemy.tscn")
+var path_enemy_scene = preload("res://scenes/path_enemy.tscn")
 
 @onready var spawn_positions = $SpawnPositions
 @onready var timeout_notifier = $Timer
@@ -20,3 +22,10 @@ func spawn_enemy():
 	var enemy_instantce = enemy_scene.instantiate()
 	enemy_instantce.global_position = random_spawn_position.global_position
 	emit_signal("enemy_spawned", enemy_instantce)
+
+func _on_path_enemy_timer_timeout():
+	spawn_path_enemy()
+
+func spawn_path_enemy():
+	var path_enemy_instance = path_enemy_scene.instantiate()
+	emit_signal("path_enemy_spawned", path_enemy_instance)
