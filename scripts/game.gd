4d4917a53fd6e03d197c3f9ad0e4577ef47cff9f
Jake Lipson <jake_lipson@yahoo.com>
2024-06-06 00:22:03 -0400

    Added game over screen and fixed bug
    Lesson 3.19
    
    - Added Game Over Screen
    - Fixed bug that would give a player points when an enemy despawned

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,13 +5,16 @@ var score = 0
 
 @onready var player = $Player
 @onready var hud = $UI/HUD
+@onready var ui = $UI
+
+var gos_scene = preload("res://scenes/game_over_screen.tscn")
 
 func _ready():
 	hud.set_score_label(score)
 	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area):
-	area.die()
+	area.queue_free()
 
 func _on_player_took_damage():
 	lives -= 1
@@ -19,6 +22,12 @@ func _on_player_took_damage():
 	if lives <= 0:
 		print("game over")
 		player.die()
+		
+		await get_tree().create_timer(1.5).timeout
+		
+		var gos = gos_scene.instantiate()
+		gos.set_score(score)
+		ui.add_child(gos)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect("died", _on_enemy_died)
